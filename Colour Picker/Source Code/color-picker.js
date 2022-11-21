71331b08c8443fee36abfea8fe4efb0af0ac80ca
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2022-11-21 15:49:19 +0530

    Uploaded source code and preview images

---
+++ Colour Picker/Source Code/color-picker.js
@@ -0,0 +1,37 @@
+function changeRed() {
+    document.getElementById("div2").style.backgroundcolor = "Red"; //container name
+                                    //css  //function       //containe-name 
+  }
+  function changeBlue() {
+    document.getElementById("div2").style.backgroundColor = "blue";
+  }
+  function changeBrown() {
+    document.getElementById("div2").style.backgroundColor = "brown";
+  }
+  function changeGreen() {
+    document.getElementById("div2").style.backgroundColor = "green";
+  }
+  function changeOrange() {
+    document.getElementById("div2").style.backgroundColor = "Orange";
+  }
+  function changeBlack() {
+    document.getElementById("div2").style.backgroundColor = "Black";
+  }
+  function changeWhite() {
+    document.getElementById("div2").style.backgroundColor = "White";
+  }
+  function changePink() {
+    document.getElementById("div2").style.backgroundColor = "Pink";
+  }
+  function changeYellow() {
+    document.getElementById("div2").style.backgroundColor = "Yellow";
+  }
+  function changePurple() {
+    document.getElementById("div2").style.backgroundColor = "Purple";
+  }
+  function changecyan() {
+    document.getElementById("div2").style.backgroundColor = "cyan";
+  }
+  function changegrey() {
+    document.getElementById("div2").style.backgroundColor = "grey";
+  }
